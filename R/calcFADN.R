754028c37a554eb57b07b7ed09316cb77a25bf04
abhimishr <mishra.abhijeet28@gmail.com>
2017-03-02 16:12:39 +0100

    Made as function.

--- R/calcFADN.R
+++ R/calcFADN.R
@@ -1,12 +1,12 @@
-calcFADN <- function(backup=T){
-  
+calcFADN <- function(){
+
   BELGIUM <- rbind(readFADN(start = 1990),readFADN(start = 1991),readFADN(start = 1992),readFADN(start = 1993),readFADN(start = 1994),
                    readFADN(start = 1995),readFADN(start = 1996),readFADN(start = 1997),readFADN(start = 1998),readFADN(start = 1999),
                    readFADN(start = 2000),readFADN(start = 2001),readFADN(start = 2002),readFADN(start = 2003),readFADN(start = 2004),
                    readFADN(start = 2005),readFADN(start = 2006),readFADN(start = 2007),readFADN(start = 2008),readFADN(start = 2009))
-  
+
   summary(BELGIUM) #Still NAs in the data
-  
+
   #Converting NAs to means
   BELGIUM$TG_Annual <- na.aggregate(BELGIUM$TG_Annual)
   BELGIUM$TG_Annual_Squared <- na.aggregate(BELGIUM$TG_Annual_Squared)
@@ -15,95 +15,95 @@ calcFADN <- function(backup=T){
   BELGIUM$R10mm_Annual <- na.aggregate(BELGIUM$R10mm_Annual)
   BELGIUM$TG90p_Annual <- na.aggregate(BELGIUM$TG90p_Annual)
   BELGIUM$SU_Annual <- na.aggregate(BELGIUM$SU_Annual)
-  
+
   summary(BELGIUM) #No NAs in the data
-  
+
   #Exploring Data
-  
+
   backup <- BELGIUM #Keeping a copy of data just in case
-  
+
   # In case you need to delete all files in Env except for the files you desire and the "analysis" function:
   # Just run the following code fragment to remove unncecessary files
-  
+
   # save_it <- c("analysis", "backup")
   #   x <- ls()
   #   x <- x[! x %in% save_it]
-  #   rm(list = x)  
+  #   rm(list = x)
   # rm(x)
   # BELGIUM <- backup
-  
-  
+
+
   BELGIUM <- subset(BELGIUM, BELGIUM$SE430>0) #keeping only positive farm income values
   BELGIUM <- subset(BELGIUM, BELGIUM$SE605>0) #Keeping only positive farm subsidies
   BELGIUM <- subset(BELGIUM, BELGIUM$SE131>0) #Keeping only positive total output
-  
+
   theme_set(theme_grey(base_size = 18))
   # Farm Incomes: Heterogeneity across YEARS and Regions
   # plotmeans draw a 95% confidence interval around the means
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
   ggplot(x,aes(YEAR,SE430,colour = Region)) + geom_smooth(span = 0.3) + facet_wrap(~NUTS3) + ggtitle("Farm family income across different regions in Belgium (1990-2009)") + labs(x="Year",y="Average income in 2009 Euro")
-  
+
   ## Weather related indicators
   ##############################
-  
+
   # Annual Temperature
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   ggplot(x, aes(x=YEAR, y=TG_Annual, colour=Region), shape=Region) + geom_point() + geom_smooth(span = 1.5) + ggtitle("Mean annual temperatures in Belgium (1990-2009)") + labs(x="Year",y="Temperature (degree Celcius)")
   p1 <- ggplot(x, aes(x=YEAR, y=TG_Annual, colour=Region), shape=Region) + geom_point() + geom_smooth(span = 1.5) + ggtitle("Mean annual temperatures in Belgium (1990-2009)") + labs(x="Year",y="Temperature (degree Celcius)")
-  
+
   # Annual Precipitation
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   ggplot(x, aes(x=YEAR, y=RR_Annual, colour=Region, shape=Region)) +  geom_point() + geom_smooth(span = 1.5) + ggtitle("Mean annual precipitation in Belgium (1990-2009)") + labs(x="Year",y="Precipitation (mm)")
   p2 <- ggplot(x, aes(x=YEAR, y=RR_Annual, colour=Region, shape=Region)) +  geom_point() + geom_smooth(span = 1.5) + ggtitle("Mean annual precipitation in Belgium (1990-2009)") + labs(x="Year",y="Precipitation (mm)")
-  
+
   #Plot on one frame
   gridExtra::grid.arrange(p1, p2, ncol=2)
-  
+
   theme_set(theme_grey(base_size = 15))
   # ETR
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   p3 <- ggplot(x, aes(x=YEAR, y=ETR_Annual, colour=Region, shape=Region)) + geom_point() + geom_smooth(span = 1.5) + ggtitle("Extreme temperature range in Belgium (1990-2009)") + labs(x="Year",y="ETR (degree Celcius)")
-  
+
   # Warm Days
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   p4 <- ggplot(x, aes(x=YEAR, y=TG90p_Annual, colour=Region, shape=Region)) + geom_point() + geom_smooth(span = 1.5) + ggtitle("Number of warm days in Belgium (1990-2009)") + labs(x="Year",y="no. of days")
-  
+
   # Heavy precipitation
-  x <- BELGIUM 
+  x <- BELGIUM
   col <-ncol(x)
   x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
   colnames(x)[col+1] <- "Region"
   p5 <- ggplot(x, aes(x=YEAR, y=R10mm_Annual, colour=Region, shape=Region)) + geom_point() + geom_smooth(span = 1.5) + ggtitle("Number of warm days in Belgium (1990-2009)") + labs(x="Year",y="no. of extreme rain days")
-  
+
   #Plot on one frame
   gridExtra::grid.arrange(p3, p4, p5,ncol=3)
-  
+
   #reset the theme
   theme_set(theme_grey(base_size = 18))
-  
-  # x <- BELGIUM 
+
+  # x <- BELGIUM
   # col <-ncol(x)
   # x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
   # x[grep("BE3",x$NUTS3),col+1] <- "Wallonia"
@@ -112,12 +112,12 @@ calcFADN <- function(backup=T){
   # ggplot(x,aes(YEAR,SE430,colour = Region)) + geom_smooth(span = 0.3) + facet_wrap(~TF14) + ggtitle("Farm family income across different regions in Belgium (1990-2009)") + labs(x="Year",y="Average income in 2009 Euro")
   # z <- ggplot(x,aes(YEAR,SE430,colour = Region)) + geom_smooth(span = 0.3) + facet_wrap(~TF14) + ggtitle("Farm family income across different regions in Belgium (1990-2009)") + labs(x="Year",y="Average income in 2009 Euro")
   # cowplot::ggdraw(cowplot::add_sub(z, "Farm codes: \n14: Other field crop; 20: Horticulture; 41:Dairy \n50: Granivore; 70: Mixed livestock; 80: Mixed crop & livestock"),)
-  # 
-  
+  #
+
   ## Mean income across years according to farm types
   ggp <- list()
   for (i in c(14,20,41,45,50,70,80)){
-    x <- subset(BELGIUM, BELGIUM$TF14==i) 
+    x <- subset(BELGIUM, BELGIUM$TF14==i)
     x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
     col <-ncol(x)
     x[grep("BE2",x$NUTS3),col+1] <- "Flanders"
@@ -128,19 +128,19 @@ calcFADN <- function(backup=T){
     name <- paste0("TC",i)
     ggp[[name]] <- ggpobject
   }
-  
+
   #Write results in a file
   folder <- "Farm Income (Specialist)"
   if (!dir.exists(folder)){
     dir.create(folder)
-  } 
-  
+  }
+
   pdf(paste0(getwd(),"/",folder,"/",folder,".pdf"),width = 11.7,height = 8.3,fonts =  "Times")
   for (i in 1:length(ggp)) {
     print(ggp[[i]])
   }
   dev.off()
-  
+
   rm(x)
   rm(i)
   rm(col)
@@ -148,16 +148,16 @@ calcFADN <- function(backup=T){
   rm(ggpobject)
   rm(folder)
   rm(name)
-  
+
   # Check for correlations
   # ======================
   folder <- "Correlation Plots"
   if (!dir.exists(folder)){
     dir.create(folder)
-  } 
-  
+  }
+
   for (i in c(14,20,41,45,50,70,80)) {
-    x <- subset(BELGIUM, BELGIUM$TF14==i) 
+    x <- subset(BELGIUM, BELGIUM$TF14==i)
     x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
     # First extract indexes of only numerical columns
     BELCorr <- x[,c("SE510","SE005","F79","SE430","SE436","DTOTLU","SE281","SE605","SE131","F81","B48","AA","AUIrr","TG_Annual","RR_Annual","ETR_Annual","R10mm_Annual","SU_Annual")]
@@ -172,17 +172,17 @@ calcFADN <- function(backup=T){
     corrplot(B_Corr,method = "number",order = "hclust",addrect = 5,mar=c(0,0,1,0),title = paste0("Coorelation Plot of Farm Code TF",i),type = "lower")
     dev.off()
   }
-  
+
   rm(nums)
   rm(BELCorr)
   rm(B_Corr)
   rm(i)
   rm(x)
   rm(folder)
-  
+
   ## Specify the regression formula
-  formula <- SE430 ~ SE510 + SE005 + F79 + SE436 + DTOTLU +  SE281  +  SE605  +  SE131 + F81  +  AA   +  AUIrr  + B48 + TG_Annual +  TG_Annual_Squared + log10(TG90p_Annual) + ETR_Annual + RR_Annual + RR_Annual_Squared + log10(R10mm_Annual) + log10(SU_Annual) 
-  
+  formula <- SE430 ~ SE510 + SE005 + F79 + SE436 + DTOTLU +  SE281  +  SE605  +  SE131 + F81  +  AA   +  AUIrr  + B48 + TG_Annual +  TG_Annual_Squared + log10(TG90p_Annual) + ETR_Annual + RR_Annual + RR_Annual_Squared + log10(R10mm_Annual) + log10(SU_Annual)
+
   #TF14 FADN Classification code summary:
   #=====================================
   #14: OtFC (Other Field Crops)
@@ -192,41 +192,41 @@ calcFADN <- function(backup=T){
   #50: GRANI (Granivores)
   #70: MIXEDLS (Mixed Livestock)
   #80: MIXEDCrLS (Mixed Crop and Livestock)
-  
+
   ## OLS estimates
-  
+
   pb <- winProgressBar(title = "OLS estimates", min = 0, max = 70, width = 300)
   OLS_List <- list()
   for (i in c(14,20,41,45,50,70,80)){
     Sys.sleep(0.1)
     setWinProgressBar(pb, i, title=paste("OLS estimates calculated for farm TF",i))
-    x <- subset(BELGIUM, BELGIUM$TF14==i) 
-    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) ) 
+    x <- subset(BELGIUM, BELGIUM$TF14==i)
+    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
     ols <- lm(formula, data = x)
     name <- paste0('TFC_',i) #TFC = FADN TF14 clalssification code. TFC_14 contains data for OtFC (Other Field Crops)....
     OLS_List[[name]] <- ols
   }
   close(pb)
-  
-  
+
+
   #################### PANEL DATA TEST ########################
-  
+
   ## Fixed effects: n entity-specific intercepts (using plm)
-  
+
   pb <- winProgressBar(title = "One way individual fixed effects", min = 0, max = 70, width = 300)
   fixef_List <- list()
   for (i in c(14,20,41,45,50,70,80)){
     Sys.sleep(0.1)
     setWinProgressBar(pb, i, title=paste("Fixef for farm TF",i))
-    x <- subset(BELGIUM, BELGIUM$TF14==i) 
-    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) ) 
+    x <- subset(BELGIUM, BELGIUM$TF14==i)
+    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
     fixef <- plm(formula, data = x, index = c("ID", "YEAR"), model="within", effect = "individual")
     name <- paste0('TFC_',i) #TFC = FADN TF14 clalssification code. TFC_14 contains data for OtFC (Other Field Crops)....
     fixef_List[[name]] <- fixef
   }
   close(pb)
   ###### OLS OR FIXED : pFtest ######
-  
+
   for (i in c(14,20,41,45,50,70,80)){
     name <- paste0('TFC_',i)
     x <- pFtest(fixef_List[[name]],OLS_List[[name]])
@@ -238,26 +238,26 @@ calcFADN <- function(backup=T){
       cat(paste0("As p value is greater than 0.05, OLS estimates are suitable for TFC_",i,".\n\n"))
     }
   }
-  
+
   ## Random effects model
-  
+
   random_List <- list()
   for (i in c(14,20,41,45,50,70,80)){
-    x <- subset(BELGIUM, BELGIUM$TF14==i) 
-    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) ) 
+    x <- subset(BELGIUM, BELGIUM$TF14==i)
+    x <- subset(x,!(x$SE430 > quantile(x$SE430, probs=c(.01, .99))[2] | x$SE430 < quantile(x$SE430, probs=c(.01, .91))[1]) )
     cat(paste0("\nRandom effects model being run for farm type TF",i,".\nWriting results in the list.\n"))
     random <- plm(formula, data = x, index = c("ID", "YEAR"), model="random", effect = "individual")
     name <- paste0('TFC_',i) #TFC = FADN TF14 clalssification code. TFC_14 contains data for OtFC (Other Field Crops)....
     random_List[[name]] <- random
   }
-  
-  
+
+
   ## Fixed or Random: Hausman test
-  
-  # To decide between fixed or random effects we can run a Hausman test 
+
+  # To decide between fixed or random effects we can run a Hausman test
   # In HAUSMAN test, the null hypothesis is that the preferred model is random effects vs. the alternative the fixed effects.
   # It basically tests whether the unique errors (ui) are correlated with the regressors, the null hypothesis is they are not.
-  
+
   #phtest to choose between random or fixed effects
   for (i in c(14,20,41,45,50,70,80)){
     name <- paste0('TFC_',i)
@@ -270,7 +270,7 @@ calcFADN <- function(backup=T){
       cat(paste0("As p value is greater than 0.05, RANDOM effects model is suitable for TFC_",i,".\n\n"))
     }
   }
-  
+
   rm(i)
   rm(ols)
   rm(fixef)
@@ -279,11 +279,11 @@ calcFADN <- function(backup=T){
   rm(pval)
   rm(x)
   rm(pb)
-  
+
   BELTemp <- BELGIUM[,c("SE510","SE005","F79","SE430","G95CV","SE436","DTOTLU","SE281","SE605","SE131","F81","B48","AA","AUIrr","TG_Annual","RR_Annual","ETR_Annual","R10mm_Annual","TG90p_Annual", "SU_Annual")]
   stargazer(BELTemp, out="Belgium Descriptors.htm",digits = 1)
   rm(BELTemp)
-  
+
   #TF14 FADN Classification code summary:
   #=====================================
   #14: OtFC (Other Field Crops)
@@ -293,39 +293,39 @@ calcFADN <- function(backup=T){
   #50: GRANI (Granivores)
   #70: MIXEDLS (Mixed Livestock)
   #80: MIXEDCrLS (Mixed Crop and Livestock)
-  
+
   stargazer(OLS_List[[1]],OLS_List[[2]],OLS_List[[3]],OLS_List[[4]],OLS_List[[5]],OLS_List[[6]],OLS_List[[7]],
             type= "html", stystyle = "default", digits = 1,
             no.space = T, single.row = T ,dep.var.labels=c("Farm family income (Euro)"),
             column.labels = c("Other Field Crops (TFC14)","Horticulture (TFC20)","Milk (TFC41)",
                               "Cattle (TFC45)","Granivores (TFC50)","Mixed LS (TFC70)","Mixed CrLS (TFC80)"),
             covariate.labels = c("Average farm capital (Euro)","Economic size (ESU)","Electricity","Total Assets ",
-                                 "Total Livestock (LU)", "Crop Specific Inputs", "Total Subsidies", "Total Crop Output", 
+                                 "Total Livestock (LU)", "Crop Specific Inputs", "Total Subsidies", "Total Crop Output",
                                  "Water", "Area Under Agriculture (ha)", "Area Under Irrigation (ha)", "UAA Owned (ha)",
-                                 "Annual mean temperature (degC)", "Annual mean temperature Sq. (degC)", 
+                                 "Annual mean temperature (degC)", "Annual mean temperature Sq. (degC)",
                                  "No. of extreme temperature days (log)", "Extreme temperature range (degC)",
                                  "Annual mean precipitation (mm)", "Annual mean precipitation Sq. (mm)", "No. of extreme rainfall days (log)","No. of Summer days (log)")
             , out="OLS Estimates.htm")
-  
+
   stargazer(fixef_List[[1]],fixef_List[[2]],fixef_List[[3]],fixef_List[[4]],fixef_List[[5]],fixef_List[[6]],fixef_List[[7]],
             type= "html", stystyle = "default", digits = 1,
             no.space = T, single.row = T ,dep.var.labels=c("Farm family income (Euro)"),
             column.labels = c("Other Field Crops (TFC14)","Horticulture (TFC20)","Milk (TFC41)",
                               "Cattle (TFC45)","Granivores (TFC50)","Mixed LS (TFC70)","Mixed CrLS (TFC80)"),
             covariate.labels = c("Average farm capital (Euro)","Economic size (ESU)","Electricity","Total Assets ",
-                                 "Total Livestock (LU)", "Crop Specific Inputs", "Total Subsidies", "Total Crop Output", 
+                                 "Total Livestock (LU)", "Crop Specific Inputs", "Total Subsidies", "Total Crop Output",
                                  "Water", "Area Under Agriculture (ha)", "Area Under Irrigation (ha)", "UAA Owned (ha)",
-                                 "Annual mean temperature (degC)", "Annual mean temperature Sq. (degC)", 
+                                 "Annual mean temperature (degC)", "Annual mean temperature Sq. (degC)",
                                  "No. of extreme temperature days (log)", "Extreme temperature range (degC)",
                                  "Annual mean precipitation (mm)", "Annual mean precipitation Sq. (mm)", "No. of extreme rainfall days (log)","No. of Summer days (log)")
             , out="fixef Models.htm")
-  
-  
+
+
   ## Plot results of panel model
   ggp <- list()
   for (i in c(14,20,41,45,50,70,80)){
     df <- subset(BELGIUM,BELGIUM$TF14==i)
-    df <- subset(df,!(df$SE430 > quantile(df$SE430, probs=c(.01, .99))[2] | df$SE430 < quantile(df$SE430, probs=c(.01, .91))[1]) ) 
+    df <- subset(df,!(df$SE430 > quantile(df$SE430, probs=c(.01, .99))[2] | df$SE430 < quantile(df$SE430, probs=c(.01, .91))[1]) )
     df$predicted <- as.numeric(fixef_List[[paste0("TFC_",i)]]$model[[1]] - fixef_List[[paste0("TFC_",i)]]$residuals)
     g <- ggplot(df)
     g <- g + geom_point(aes(x=SE430, y = predicted), size = 2, colour = "blue")
@@ -334,19 +334,19 @@ calcFADN <- function(backup=T){
     name <- paste0("TC",i)
     ggp[[name]] <- ggpobject
   }
-  
+
   #Write results in a file
   folder <- "Panel Model Performance Plots"
   if (!dir.exists(folder)){
     dir.create(folder)
-  } 
-  
+  }
+
   pdf(paste0(getwd(),"/",folder,"/",folder,".pdf"),width = 11.7,height = 8.3,fonts =  "Times")
   for (i in 1:length(ggp)) {
     print(ggp[[i]])
   }
   dev.off()
-  
+
   ## Plot results of OLS estimates
   ggp_ols <- list()
   for (i in c(14,20,41,45,50,70,80)){
@@ -360,19 +360,19 @@ calcFADN <- function(backup=T){
     name <- paste0("TC",i)
     ggp_ols[[name]] <- ggpobject
   }
-  
+
   #Write results in a file
   folder <- "OLS Performance Plots"
   if (!dir.exists(folder)){
     dir.create(folder)
-  } 
-  
+  }
+
   pdf(paste0(getwd(),"/",folder,"/",folder,".pdf"),width = 11.7,height = 8.3,fonts =  "Times")
   for (i in 1:length(ggp_ols)) {
     print(ggp_ols[[i]])
   }
   dev.off()
-  
+
   rm(g)
   rm(folder)
   rm(ggp)
@@ -381,5 +381,5 @@ calcFADN <- function(backup=T){
   rm(i)
   rm(name)
   rm(df)
-  
+
 }
